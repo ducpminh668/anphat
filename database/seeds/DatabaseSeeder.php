656a29f0f74b1e5f12d453a62bffc214c9ec739b
Duc Pham <ducpminh668@gmail.com>
2021-06-14 22:28:39 +0700

    init

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Seeder\LaratrustSeeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+        $this->call(\LaratrustSeeder::class);
+    }
+}
