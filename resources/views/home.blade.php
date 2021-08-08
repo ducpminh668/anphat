b7620f1ffd02c0d07b12749fec9d0411b71dbd2b
macbook <macbook@DucPhams-MacBook-Pro.local>
2021-08-08 20:33:42 +0700

    d

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,62 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+<div class="row">
+    <div class="col-sm-6 col-xl-3">
+        <div class="card card-body bg-blue-400 has-bg-image">
+            <div class="media">
+                <div class="media-body">
+                    <h3 class="mb-0">54,390</h3>
+                    <span class="text-uppercase font-size-xs">Khách hàng mới</span>
+                </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                <div class="ml-3 align-self-center">
+                    <i class="icon-bubbles4 icon-3x opacity-75"></i>
+                </div>
+            </div>
+        </div>
+    </div>
 
-                    You are logged in!
+    <div class="col-sm-6 col-xl-3">
+        <div class="card card-body bg-danger-400 has-bg-image">
+            <div class="media">
+                <div class="media-body">
+                    <h3 class="mb-0">389,438</h3>
+                    <span class="text-uppercase font-size-xs">Tổng đơn hàng trong tháng</span>
+                </div>
+
+                <div class="ml-3 align-self-center">
+                    <i class="icon-bag icon-3x opacity-75"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-6 col-xl-3">
+        <div class="card card-body bg-success-400 has-bg-image">
+            <div class="media">
+                <div class="mr-3 align-self-center">
+                    <i class="icon-pointer icon-3x opacity-75"></i>
+                </div>
+
+                <div class="media-body text-right">
+                    <h3 class="mb-0">652,549</h3>
+                    <span class="text-uppercase font-size-xs">Doanh thu tháng</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-6 col-xl-3">
+        <div class="card card-body bg-indigo-400 has-bg-image">
+            <div class="media">
+                <div class="mr-3 align-self-center">
+                    <i class="icon-enter6 icon-3x opacity-75"></i>
+                </div>
+
+                <div class="media-body text-right">
+                    <h3 class="mb-0">245,382</h3>
+                    <span class="text-uppercase font-size-xs">Lợi nhuận ròng</span>
                 </div>
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,176 +64,19 @@
                     </a>
                 </li>
 
-                <li class="nav-item dropdown">
-                    <a href="#" class="navbar-nav-link dropdown-toggle caret-0" data-toggle="dropdown">
-                        <i class="icon-git-compare"></i>
-                        <span class="d-md-none ml-2">Git updates</span>
-                        <span class="badge badge-pill bg-warning-400 ml-auto ml-md-0">9</span>
-                    </a>
 
-                    <div class="dropdown-menu dropdown-content wmin-md-350">
-                        <div class="dropdown-content-header">
-                            <span class="font-weight-semibold">Git updates</span>
-                            <a href="#" class="text-default"><i class="icon-sync"></i></a>
-                        </div>
-
-                        <div class="dropdown-content-body dropdown-scrollable">
-                            <ul class="media-list">
-                                <li class="media">
-                                    <div class="mr-3">
-                                        <a href="#" class="btn bg-transparent border-primary text-primary rounded-round border-2 btn-icon"><i class="icon-git-pull-request"></i></a>
-                                    </div>
-
-                                    <div class="media-body">
-                                        Drop the IE <a href="#">specific hacks</a> for temporal inputs
-                                        <div class="text-muted font-size-sm">4 minutes ago</div>
-                                    </div>
-                                </li>
-
-                                <li class="media">
-                                    <div class="mr-3">
-                                        <a href="#" class="btn bg-transparent border-warning text-warning rounded-round border-2 btn-icon"><i class="icon-git-commit"></i></a>
-                                    </div>
-
-                                    <div class="media-body">
-                                        Add full font overrides for popovers and tooltips
-                                        <div class="text-muted font-size-sm">36 minutes ago</div>
-                                    </div>
-                                </li>
-
-                                <li class="media">
-                                    <div class="mr-3">
-                                        <a href="#" class="btn bg-transparent border-info text-info rounded-round border-2 btn-icon"><i class="icon-git-branch"></i></a>
-                                    </div>
-
-                                    <div class="media-body">
-                                        <a href="#">Chris Arney</a> created a new <span class="font-weight-semibold">Design</span> branch
-                                        <div class="text-muted font-size-sm">2 hours ago</div>
-                                    </div>
-                                </li>
-
-                                <li class="media">
-                                    <div class="mr-3">
-                                        <a href="#" class="btn bg-transparent border-success text-success rounded-round border-2 btn-icon"><i class="icon-git-merge"></i></a>
-                                    </div>
-
-                                    <div class="media-body">
-                                        <a href="#">Eugene Kopyov</a> merged <span class="font-weight-semibold">Master</span> and <span class="font-weight-semibold">Dev</span> branches
-                                        <div class="text-muted font-size-sm">Dec 18, 18:36</div>
-                                    </div>
-                                </li>
-
-                                <li class="media">
-                                    <div class="mr-3">
-                                        <a href="#" class="btn bg-transparent border-primary text-primary rounded-round border-2 btn-icon"><i class="icon-git-pull-request"></i></a>
-                                    </div>
-
-                                    <div class="media-body">
-                                        Have Carousel ignore keyboard events
-                                        <div class="text-muted font-size-sm">Dec 12, 05:46</div>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-
-                        <div class="dropdown-content-footer bg-light">
-                            <a href="#" class="text-grey mr-auto">All updates</a>
-                            <div>
-                                <a href="#" class="text-grey" data-popup="tooltip" title="Mark all as read"><i class="icon-radio-unchecked"></i></a>
-                                <a href="#" class="text-grey ml-2" data-popup="tooltip" title="Bug tracker"><i class="icon-bug2"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </li>
             </ul>
 
             <span class="navbar-text ml-md-3 mr-md-auto">
-                <span class="badge bg-success">Online</span>
+                <!-- <span class="badge bg-success">Online</span> -->
             </span>
 
             <ul class="navbar-nav">
-                <li class="nav-item dropdown">
-                    <a href="#" class="navbar-nav-link dropdown-toggle caret-0" data-toggle="dropdown">
-                        <i class="icon-people"></i>
-                        <span class="d-md-none ml-2">Users</span>
-                    </a>
-
-                    <div class="dropdown-menu dropdown-menu-right dropdown-content wmin-md-300">
-                        <div class="dropdown-content-header">
-                            <span class="font-weight-semibold">Users online</span>
-                            <a href="#" class="text-default"><i class="icon-search4 font-size-base"></i></a>
-                        </div>
-
-                        <div class="dropdown-content-body dropdown-scrollable">
-                            <ul class="media-list">
-                                <li class="media">
-                                    <div class="mr-3">
-                                        <img src="/admin/global_assets/images/placeholders/placeholder.jpg" width="36" height="36" class="rounded-circle" alt="">
-                                    </div>
-                                    <div class="media-body">
-                                        <a href="#" class="media-title font-weight-semibold">Jordana Ansley</a>
-                                        <span class="d-block text-muted font-size-sm">Lead web developer</span>
-                                    </div>
-                                    <div class="ml-3 align-self-center"><span class="badge badge-mark border-success"></span></div>
-                                </li>
-
-                                <li class="media">
-                                    <div class="mr-3">
-                                        <img src="/admin/global_assets/images/placeholders/placeholder.jpg" width="36" height="36" class="rounded-circle" alt="">
-                                    </div>
-                                    <div class="media-body">
-                                        <a href="#" class="media-title font-weight-semibold">Will Brason</a>
-                                        <span class="d-block text-muted font-size-sm">Marketing manager</span>
-                                    </div>
-                                    <div class="ml-3 align-self-center"><span class="badge badge-mark border-danger"></span></div>
-                                </li>
-
-                                <li class="media">
-                                    <div class="mr-3">
-                                        <img src="/admin/global_assets/images/placeholders/placeholder.jpg" width="36" height="36" class="rounded-circle" alt="">
-                                    </div>
-                                    <div class="media-body">
-                                        <a href="#" class="media-title font-weight-semibold">Hanna Walden</a>
-                                        <span class="d-block text-muted font-size-sm">Project manager</span>
-                                    </div>
-                                    <div class="ml-3 align-self-center"><span class="badge badge-mark border-success"></span></div>
-                                </li>
-
-                                <li class="media">
-                                    <div class="mr-3">
-                                        <img src="/admin/global_assets/images/placeholders/placeholder.jpg" width="36" height="36" class="rounded-circle" alt="">
-                                    </div>
-                                    <div class="media-body">
-                                        <a href="#" class="media-title font-weight-semibold">Dori Laperriere</a>
-                                        <span class="d-block text-muted font-size-sm">Business developer</span>
-                                    </div>
-                                    <div class="ml-3 align-self-center"><span class="badge badge-mark border-warning-300"></span></div>
-                                </li>
-
-                                <li class="media">
-                                    <div class="mr-3">
-                                        <img src="/admin/global_assets/images/placeholders/placeholder.jpg" width="36" height="36" class="rounded-circle" alt="">
-                                    </div>
-                                    <div class="media-body">
-                                        <a href="#" class="media-title font-weight-semibold">Vanessa Aurelius</a>
-                                        <span class="d-block text-muted font-size-sm">UX expert</span>
-                                    </div>
-                                    <div class="ml-3 align-self-center"><span class="badge badge-mark border-grey-400"></span></div>
-                                </li>
-                            </ul>
-                        </div>
-
-                        <div class="dropdown-content-footer bg-light">
-                            <a href="#" class="text-grey mr-auto">All users</a>
-                            <a href="#" class="text-grey"><i class="icon-gear"></i></a>
-                        </div>
-                    </div>
-                </li>
 
                 <li class="nav-item dropdown">
                     <a href="#" class="navbar-nav-link dropdown-toggle caret-0" data-toggle="dropdown">
-                        <i class="icon-bubbles4"></i>
-                        <span class="d-md-none ml-2">Messages</span>
+                        <i class="icon-bell3"></i>
+                        <span class="d-md-none ml-2">Thông báo</span>
                         <span class="badge badge-pill bg-warning-400 ml-auto ml-md-0">2</span>
                     </a>
 
@@ -329,16 +172,16 @@
                             </ul>
                         </div>
 
-                        <div class="dropdown-content-footer justify-content-center p-0">
+                        <!-- <div class="dropdown-content-footer justify-content-center p-0">
                             <a href="#" class="bg-light text-grey w-100 py-2" data-popup="tooltip" title="Load more"><i class="icon-menu7 d-block top-0"></i></a>
-                        </div>
+                        </div> -->
                     </div>
                 </li>
 
                 <li class="nav-item dropdown dropdown-user">
                     <a href="#" class="navbar-nav-link dropdown-toggle" data-toggle="dropdown">
                         <img src="/admin/global_assets/images/placeholders/placeholder.jpg" class="rounded-circle" alt="">
-                        <span>Victoria</span>
+                        <span>{{Auth::user()->name}}</span>
                     </a>
 
                     <div class="dropdown-menu dropdown-menu-right">
@@ -360,7 +203,7 @@
     <div class="page-content">
 
         <!-- Main sidebar -->
-       @include('includes.main_sidebar')
+        @include('includes.main_sidebar')
         <!-- /main sidebar -->
 
 
@@ -368,7 +211,7 @@
         <div class="content-wrapper">
 
             <!-- Page header -->
-            <div class="page-header page-header-light">
+            <!-- <div class="page-header page-header-light">
                 <div class="page-header-content header-elements-md-inline">
                     <div class="page-title d-flex">
                         <h4><i class="icon-arrow-left52 mr-2"></i> <span class="font-weight-semibold">Alpaca</span> - Basic Inputs</h4>
@@ -419,71 +262,24 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> -->
             <!-- /page header -->
 
 
             <!-- Content area -->
             <div class="content">
-
+                
                 <!-- Text inputs title -->
-                <div class="mb-3">
+                <!-- <div class="mb-3">
                     <h6 class="mb-0 font-weight-semibold">
                         Text inputs
                     </h6>
                     <span class="text-muted d-block">Input fields with options</span>
-                </div>
+                </div> -->
                 <!-- /text inputs title -->
 
                 <!-- Basic examples -->
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="card">
-                            <div class="card-header header-elements-inline">
-                                <h6 class="card-title">Basic text field</h6>
-                                <div class="header-elements">
-                                    <div class="list-icons">
-                                        <a class="list-icons-item" data-action="collapse"></a>
-                                        <a class="list-icons-item" data-action="reload"></a>
-                                        <a class="list-icons-item" data-action="remove"></a>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="card-body">
-                                <p class="mb-3">A simple example of using Alpaca with nothing more than a string of text. Alpaca looks at your data and determines that it is a string. It then looks for a suitable candidate for representing a string and it decides to use the <code>text</code> field.</p>
-                                <div id="alpaca-basic"></div>
-                                <a href="#alpaca-basic-source" data-toggle="collapse"><i class="icon-embed2 mr-2"></i> Show source code</a>
-                                <div class="collapse mt-2" id="alpaca-basic-source">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-md-6">
-                        <div class="card">
-                            <div class="card-header header-elements-inline">
-                                <h6 class="card-title">Display only view</h6>
-                                <div class="header-elements">
-                                    <div class="list-icons">
-                                        <a class="list-icons-item" data-action="collapse"></a>
-                                        <a class="list-icons-item" data-action="reload"></a>
-                                        <a class="list-icons-item" data-action="remove"></a>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="card-body">
-                                <p class="mb-3">Displays a text field using a display-only view. The text field simply prints out and is not editable, a simple block appears instead. This is a default form control type provided by Bootstrap framework.</p>
-                                <div id="alpaca-static"></div>
-                                <a href="#alpaca-static-source" data-toggle="collapse"><i class="icon-embed2 mr-2"></i> Show source code</a>
-                                <div class="collapse mt-2" id="alpaca-static-source">
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                @yield('content')
                 <!-- /basic examples -->
 
 
